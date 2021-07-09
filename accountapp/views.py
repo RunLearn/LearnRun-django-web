f5fd1a8bcf083eb0aac480c6f34b1d54e14d7b51
seojaeyong <wg7363@naver.com>
2021-07-09 12:48:35 +0900

    2021.07.09 db 여는법 까지 배웠응

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from accountapp.models import NewModel
+
+
 def account(request):
     if request.method == "POST":
+
+        temp =request.POST.get('input_text')
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
         return render(request, 'accountapp/base.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'new_model' : new_model})
     else:
         return render(request, 'accountapp/base.html',
                       context={'text': 'GET METHOD!'})

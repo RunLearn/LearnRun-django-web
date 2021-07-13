cb780babb5f8c043b9f41cd2662cd826443edae2
seojaeyong <wg7363@naver.com>
2021-07-13 12:41:23 +0900

    2021.07.13 createview

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import account
+from accountapp.views import account, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('Iam/', account, name = 'account')
+
+    path('create/',AccountCreateView.as_view(), name = 'create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -18,3 +21,9 @@ def account(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/base.html',
                       context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:account')
+    template_name = 'accountapp/create.html'

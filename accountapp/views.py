f629b200e483f9fec2d046e64f1c707ade4c5e14
seojaeyong <wg7363@naver.com>
2021-07-13 11:18:28 +0900

    2021.07.09 버튼추가했음!@!#!$@!#

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,8 +12,10 @@ def account(request):
         new_model.text = temp
         new_model.save()
 
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/base.html',
-                      context={'new_model' : new_model})
+                      context={'data_list' : data_list})
     else:
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/base.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'data_list': data_list})

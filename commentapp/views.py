f26df5a6fe127e049d81e3712d11bd42dd55cb65
RunLearn <wg7363@naver.com>
2021-08-12 11:43:32 +0900

    8/11 수업결석함

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
-from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorator import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
-
+@method_decorator(login_required(login_url= reverse_lazy('accountapp:login')), 'get')
+@method_decorator(login_required(login_url= reverse_lazy('accountapp:login')), 'post')
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
@@ -21,3 +25,13 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',kwargs={'pk': self.object.article.pk})

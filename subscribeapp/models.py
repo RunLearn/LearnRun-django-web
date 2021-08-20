32e837efdd391e5a75fdc4204a03384bb6b3005c
RunLearn <wg7363@naver.com>
2021-08-20 16:45:41 +0900

    8/20 commit 1

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription',
+                             null=True)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription',
+                                null=True)
+
+    class Meta:
+        unique_together = ['user','project']

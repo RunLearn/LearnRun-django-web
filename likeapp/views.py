ea72039246bcf96e40d77f5efeb5775ba6a99103
RunLearn <wg7363@naver.com>
2021-08-25 17:09:45 +0900

    8/25 commit 2

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -11,29 +13,42 @@ from django.views.generic import RedirectView
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
+@transaction.atomic
+def db_transaction(user,article):
+
+    like_record = LikeRecord.objects.filter(user=user,
+                                            article=article)
+
+    if like_record.exists():
+        # 좋아요 반영 X
+        raise ValidationError('like already exists')
+    else:
+        # 좋아요 반영 O
+        LikeRecord(user=user,
+                   article=article).save()
+
+    article.like += 1
+    article.save()
+
+
+
+
+
 @method_decorator(login_required(login_url= reverse_lazy('accountapp:login')), 'get')
 class LikeArticleView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record = LikeRecord.objects.filter(user=user,
-                                                article=article)
-
-        if like_record.exists():
-            #좋아요 반영 X
-            messages.add_message(request,messages.ERROR, '좋아요는 한번만☝')
-            return HttpResponseRedirect(reverse('articleapp:detail',
-                                                kwargs={'pk':kwargs['article_pk']}))
-        else:
+        try:
+            db_transaction(user,article)
             #좋아요 반영 O
-            LikeRecord(user=user,
-                       article=article).save()
-
-        article.like += 1
-        article.save()
-
-        messages.add_message(request, messages.SUCCESS, '좋아요는 사랑입니다💖')
+            messages.add_message(request, messages.SUCCESS, '좋아요는 사랑입니다💖')
+        except ValidationError:
+            #좋아요 반영 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만☝')
+            return HttpResponseRedirect(reverse('articleapp:detail',
+                                                kwargs={'pk': kwargs['article_pk']}))
 
         return super().get(request,*args,**kwargs)
 
